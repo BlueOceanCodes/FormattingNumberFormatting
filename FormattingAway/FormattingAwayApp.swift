7ef2a738114590e211d83bb9a6b38a0e9d9f1075
Alex Hudson <AHudson@tql.com>
2023-06-27 16:44:10 -0400

    Initial Commit

---
+++ FormattingAway/FormattingAwayApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormattingAwayApp.swift
+//  FormattingAway
+//
+//  Created by Alex Hudson on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct FormattingAwayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
